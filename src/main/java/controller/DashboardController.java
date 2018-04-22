812ba7f92d4076dc0d3d0165c7a700e893d0ffe7
bakhodirsbox <bahodir9293@gmail.com>
2018-04-22 15:06:36 -0500

    sign up part is being built

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "DashboardController")
+@WebServlet(name = "/dashboard")
 public class DashboardController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
--- src/main/java/controller/RegUserController.java
+++ src/main/java/controller/RegUserController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.User;
 import service.AuthService;
+import util.Util;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @WebServlet("/register")
@@ -38,11 +38,13 @@ public class RegUserController extends HttpServlet {
         String password = req.getParameter("password");
         Date dob = null;
         try {
-            dob = new SimpleDateFormat("dd/MM/yyyy").parse(req.getParameter("dob"));
+            dob = Util.parseToDate(req.getParameter("dob"));
         } catch (ParseException e) {
-            e.printStackTrace();
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
         }
-
-        this.authService.register(new User(userName, password, firstName, lastName, dob));
+        User newUser = new User(userName, password, firstName, lastName, dob);
+        this.authService.register(newUser);
+        req.getSession().setAttribute("currentUser", newUser);
+        resp.sendRedirect("welcome-dashboard.jsp");
     }
 }
